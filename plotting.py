0d7da8b51e958e405c6bc8d43a79ba567c8e6112
LabradotCat <zhengtingjun@outlook.com>
2021-12-06 13:48:22 +0000

    add applied field plotting method

--- plotting.py
+++ plotting.py
@@ -21,14 +21,25 @@ def load_summary(file, output):
     summary = {
         'parameters': parameters,
         'fieldloops': fieldloops,
-        'q' : q,
+        'q': q,
         'mag': mag,
         'monopole': monopole,
         'vortex_count': vortex_count,
         'macrospin_count': macrospin_count
-    }.get(output, Exception('type of statistics not included in summary'))
-
+    }.get(output, None)
     return summary
+
+def plot_applied_field(field):
+    # plotting external field
+    steps = np.arange(0, len(field), 1)
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Steps')
+    ax.set_ylabel('Applied Field (mT)')
+    ax.set_title('Applied Field on the Lattice')
+    ax.plot(steps, 1000 * field, 'k-')
+    # ax.plot(int(file[file.find('counter') + 7:file.find(r'_Loop')]), H_applied,
+    #          marker='o', markersize=3, markerfacecolor='red', markeredgecolor='red')
+    plt.show()
 
 def plot_vortex_macrospin_number(vortex_count, macrospin_count):
     '''
@@ -40,10 +51,11 @@ def plot_vortex_macrospin_number(vortex_count, macrospin_count):
     steps = np.arange(0, size, 1)
     def exp_dec(x, a, b):
         return a * (1 - np.exp(-b * x))
-    def exp_decay(x, a, b):
-        return a * (np.exp(-b * x))
+    def exp_decay(x, a, b, c):
+        return a * np.exp(-b * x) + c
+
     popt1, pcov1 = curve_fit(exp_dec, steps, vortex_count, p0=[np.max(vortex_count), 1])
-    popt2, pcov2 = curve_fit(exp_decay, steps, macrospin_count, p0=[np.max(macrospin_count)/2, 1])
+    popt2, pcov2 = curve_fit(exp_decay, steps, macrospin_count, p0=[np.max(macrospin_count)/2, 1, np.max(macrospin_count)/2])
 
     fig, ax = plt.subplots()
     ax.set_title('Evolution of Vortex and Macrospin Modes')
@@ -51,15 +63,18 @@ def plot_vortex_macrospin_number(vortex_count, macrospin_count):
     ax.set_ylabel('number of vortices')
     ax.set_xlim(0, size + 10)
     ax.set_ylim(0, np.max(macrospin_count) + 5)
-    ax.plot(steps, vortex_count, 'o')
-    ax.plot(steps, exp_dec(steps, *popt1), 'r-')
-    ax.plot(steps, macrospin_count, 'o')
-    ax.plot(steps, exp_decay(steps, *popt2), 'g-')
+    ax.plot(steps, vortex_count, 'o', label = 'number of vortices (Nv)')
+    ax.plot(steps, exp_dec(steps, *popt1), 'r--', label = 'Nv fitted')
+    ax.plot(steps, macrospin_count, 'o', label = 'number of macrospins (Nm)')
+    ax.plot(steps, exp_decay(steps, *popt2), 'b--', label = 'Nm fitted')
+    ax.legend()
     ax.grid()
     plt.show()
 
-file = 'E:\ASI_MSci_Project\ASVI_Simulation_Output\RPMStateInfo_Hmax2.425000e-02_steps10_Angle7.853982e-01_neighbours2_Loops20'
-
-vc = load_summary(file, output='vortex_count')
-mc = load_summary(file, output='macrospin_count')
-plot_vortex_macrospin_number(vc,mc)
+if __name__ == '__main__':
+    file = 'E:\ASI_MSci_Project\ASVI_Simulation_Output\RPMStateInfo_Hmax2.250000e-02_steps10_Angle7.853982e-01_neighbours2_Loops30'
+    vc = load_summary(file, output='vortex_count')
+    mc = load_summary(file, output='macrospin_count')
+    fd = load_summary(file, output='fieldloops')
+    plot_applied_field(fd)
+    plot_vortex_macrospin_number(vc,mc)
