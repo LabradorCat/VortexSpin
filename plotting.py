a191c5fc2c908f5625186f33265b74e40a84cd40
LabradotCat <zhengtingjun@outlook.com>
2021-12-10 16:05:21 +0000

    add sigmoid fit methods to track vortex to macrospin transformation

--- plotting.py
+++ plotting.py
@@ -26,11 +26,11 @@ def load_summary(file, output):
     '''
         load in statistical summary from field sweep
     '''
-    assert 'RPMStateInfo' in file   # only allow summary file input
+    assert 'ASVIStateInfo' in file  # only allow summary file input
 
     if '.npz' not in file:
         file = file + '.npz'
-    npzfile = np.load(file, allow_pickle = True)
+    npzfile = np.load(file, allow_pickle=True)
     parameters = npzfile['arr_0']
     fieldloops = npzfile['arr_1']
     q = npzfile['arr_2']
@@ -61,32 +61,44 @@ def plot_applied_field(field):
     #          marker='o', markersize=3, markerfacecolor='red', markeredgecolor='red')
     plt.show()
 
-def plot_vortex_macrospin_number(vortex_count, macrospin_count):
+def plot_vortex_macrospin_number(vortex_count, macrospin_count, fitfunc):
     '''
     Plot the time evolution of vortex numbers and macrospin numbers
     '''
     assert len(vortex_count) == len(macrospin_count)
+    assert 'exp' or 'sigmoid' in fitfunc
 
     size = len(vortex_count)
     steps = np.arange(0, size, 1)
-    def exp_dec(x, a, b):
-        return a * (1 - np.exp(-b * x))
-    def exp_decay(x, a, b, c):
-        return a * np.exp(-b * x) + c
 
-    popt1, pcov1 = curve_fit(exp_dec, steps, vortex_count, p0=[np.max(vortex_count), 1])
-    popt2, pcov2 = curve_fit(exp_decay, steps, macrospin_count, p0=[np.max(macrospin_count)/2, 1, np.max(macrospin_count)/2])
+    def exp_decay(x, a, b, c, d):
+        return a * np.exp(-b * x + c) + d
+
+    def sigmoid(x, a, b, c, d):
+        return a/(1 + np.exp(-b * x + c)) + d
 
     fig, ax = plt.subplots()
+    if fitfunc == 'exp':
+        popt1, pcov1 = curve_fit(exp_decay, steps, vortex_count, p0=[np.max(vortex_count), 1, 1, 1])
+        popt2, pcov2 = curve_fit(exp_decay, steps, macrospin_count,
+                                 p0=[np.max(macrospin_count) / 2, 1, np.max(macrospin_count) / 2, 1])
+        ax.plot(steps, exp_decay(steps, *popt1), 'r--', label='Nv fitted')
+        ax.plot(steps, exp_decay(steps, *popt2), 'b--', label='Nm fitted')
+
+    if fitfunc == 'sigmoid':
+        popt1, pcov1 = curve_fit(sigmoid, steps, vortex_count, p0=[np.max(vortex_count), 1, 1, 1])
+        popt2, pcov2 = curve_fit(sigmoid, steps, macrospin_count,
+                                 p0=[np.max(macrospin_count) / 2, 1, np.max(macrospin_count) / 2, 1])
+        ax.plot(steps, sigmoid(steps, *popt1), 'r--', label='Nv fitted')
+        ax.plot(steps, sigmoid(steps, *popt2), 'b--', label='Nm fitted')
+
     ax.set_title('Evolution of Vortex and Macrospin Modes')
     ax.set_xlabel('steps')
     ax.set_ylabel('number of vortices')
     ax.set_xlim(0, size + 10)
     ax.set_ylim(0, np.max(macrospin_count) + 5)
-    ax.plot(steps, vortex_count, 'o', label = 'number of vortices (Nv)')
-    ax.plot(steps, exp_dec(steps, *popt1), 'r--', label = 'Nv fitted')
-    ax.plot(steps, macrospin_count, 'o', label = 'number of macrospins (Nm)')
-    ax.plot(steps, exp_decay(steps, *popt2), 'b--', label = 'Nm fitted')
+    ax.plot(steps, vortex_count, 'o', label='number of vortices (Nv)')
+    ax.plot(steps, macrospin_count, 'o', label='number of macrospins (Nm)')
     ax.legend()
     ax.grid()
     plt.show()
@@ -114,7 +126,7 @@ def plot_vector_field_2D(lattice, ax):
     Cv = lattice[:, :, 10].flatten()
     # sorting out colors and thicknesses
     line_w = 4 * (bar_w > 100e-9) + 1
-    line_rbg = np.arctan(Mx+My)
+    line_rbg = np.arctan(Mx + My)
     color = cmap(norm(line_rbg))
     ax.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=1, pivot='mid', zorder=1,
               linewidths=line_w, color=color, edgecolors=color)
@@ -126,9 +138,9 @@ def plot_vector_field_2D(lattice, ax):
     return ax
 
 if __name__ == '__main__':
-    file = 'E:\ASI_MSci_Project\ASVI_Simulation_Output\RPMStateInfo_Hmax2.250000e-02_steps10_Angle7.853982e-01_neighbours2_Loops30'
+    file = 'D:\ASI_MSci_Project\ASVI_Simulation_Output\ASVIStateInfo_Hmax0.030_steps100_Angle45_n2_Loops0.npz'
     vc = load_summary(file, output='vortex_count')
     mc = load_summary(file, output='macrospin_count')
     fd = load_summary(file, output='fieldloops')
     plot_applied_field(fd)
-    plot_vortex_macrospin_number(vc,mc)
+    plot_vortex_macrospin_number(vc, mc, 'sigmoid')
