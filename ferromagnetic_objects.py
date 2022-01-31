680abce4d0f5c7d48f7454f02a5d492f3c6ae5f6
LabradotCat <zhengtingjun@outlook.com>
2022-01-31 17:46:40 +0000

    create NanoBar class as a subclass of np.ndarray

--- ferromagnetic_objects.py
+++ ferromagnetic_objects.py
@@ -1,42 +1,43 @@
-import time
-
 import numpy as np
-import time as t
 
 
-class MacroSpin():
+class NanoBar(np.ndarray):
     """
-    Object class for macrospin, a nano-scale ferromagnetic material that has a single-domain magnetic dipole
+     Object class for a nano-scale ferromagnetic material in a bar shape
     """
-    def __init__(self, pos, mag, Hc_mean = 0.025, Hc_std = 0.05,
-                 bar_length = 220e-9, bar_thickness = 25e-9, bar_width = 80e-9):
-        assert hasattr(pos, '__len__')
-        assert hasattr(pos, '__len__')
-        assert len(pos) == len(mag)
+    def __new__(cls, xpos, ypos, zpos, xmag, ymag, zmag,
+                hc_m=0.025, hc_s=0.05, bar_l=220e-9, bar_w=25e-9, bar_t=80e-9,
+                type='macrospin'):
+        hc = np.random.normal(loc=hc_m, scale=hc_s * hc_m, size=None)
+        obj = np.asarray([xpos, ypos, zpos, xmag, ymag, zmag, hc, bar_l, bar_w, bar_t]).view(cls)
+        obj.type = type
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None: return
         # Positional properties
-        self.pos = np.array(pos)
-        # Dimensional properties
-        self.bar_l = bar_length
-        self.bar_w = bar_width
-        self.bar_t = bar_thickness
+        self.pos = np.array([obj[0], obj[1], obj[2]])
         # Magnetic properties
-        self.mag = np.array(mag)
-        self.Hc_mean = Hc_mean
-        self.Hc_std = Hc_std
-        self.Hc = np.random.normal(loc=Hc_mean, scale=Hc_std * Hc_mean, size=None)
+        self.mag = np.array([obj[3], obj[4], obj[5]])
+        self.hc = obj[6]
+        self.type = getattr(obj, 'type', 'macrospin')
+        # Dimensional properties
+        self.bar_l = obj[7]
+        self.bar_w = obj[8]
+        self.bar_t = obj[9]
 
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):  # this method is called whenever you use a ufunc
+        f = {
+            "reduce": ufunc.reduce,
+            "accumulate": ufunc.accumulate,
+            "reduceat": ufunc.reduceat,
+            "outer": ufunc.outer,
+            "at": ufunc.at,
+            "__call__": ufunc,
+        }
+        output = NanoBar(f[method](*(i.view(np.ndarray) for i in inputs),**kwargs))
+        # convert the inputs to np.ndarray to prevent recursion, call the function, then cast it back as ExampleTensor
+        output.__dict__ = self.__dict__  # carry forward attributes
+        return output
     # CLASS GETTER & SETTER
-    def pos(self):
-        return self.pos
 
-    def mag(self):
-        return self.mag
-
-    def bar_length(self):
-        return self.bar_l
-
-    def bar_width(self):
-        return self.bar_w
-
-    def bar_thickness(self):
-        return self.bar_t
